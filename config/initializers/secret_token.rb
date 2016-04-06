2e1ca32ab2a441d97965e5e1ad37c287c4ce27d2
dhunnapotha <gautam4you@gmail.com>
2016-04-06 20:09:04 +0530

    Baseline

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RablTryout3222::Application.config.secret_token = '85ec72c369a2cffb68d321c90c7b6d0aa4d48273e68f0c989f684a18a2cd3d3a1ff28942c18b18f2dfe9b1993aa4d9efd0d484d607755780c9f0c80c2e63ac0f'
